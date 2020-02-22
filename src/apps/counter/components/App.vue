17731693ca2877a03fd07fca8adc7c7d7145ccbc
蒋勇 <jiangy@highzap.com>
2020-02-22 16:04:23 +0800

    添加异步测试用例

--- src/apps/counter/components/App.vue
+++ src/apps/counter/components/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="counter">Counter</div>
+  <div class="counter">
+    <div class="count">{{count}}</div>
+    <button class="increment" type="button" @click="increment">自增</button>
+    <button class="incrementAsync" type="button" @click="incrementAsync">异步自增</button>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"Counter"
+  name: "Counter",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    incrementAsync() {
+      setTimeout(() => {
+        this.count++;
+      }, 1000);
+    }
+  }
 };
 </script>
--- src/apps/counter/tests/unit/app.spec.js
+++ src/apps/counter/tests/unit/app.spec.js
@@ -2,8 +2,26 @@ import App from '../../components/App.vue'
 import { mount } from '@vue/test-utils'
 
 describe("App.vue", () => {
+
+    const factory = () => {
+        const wrapper = mount(App);
+        return wrapper;
+    }
+
     it("render", () => {
-        const warpper = mount(App);
-        expect(warpper.find('.counter').exists()).toBe(true);
+        const wrapper = factory();
+        expect(wrapper.find('.counter').exists()).toBe(true);
+    })
+
+
+    it("count异步更新, count的值应该为1", done => {
+        const wrapper = factory();
+        // 触发异步自增
+        wrapper.find('.incrementAsync').trigger('click')
+        // 1 s 后断言
+        window.setTimeout(() => {
+            expect(wrapper.vm.count).toBe(1);
+            done();
+        }, 1000)
     })
 })
